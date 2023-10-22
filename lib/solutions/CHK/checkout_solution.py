721f0b1b5b7022064d9a2bf7c408461433bade33
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2023-10-22 10:03:10 +0100

    Sun Oct 22 10:03:10 WEST 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from math import floor
 
 AVAILABLE_ITEMS = {
     "A": 50,
@@ -20,6 +21,32 @@ def checkout(skus):
     print(skus)
     items = skus.split(" ")
 
+    for item in items:
+        if item not in AVAILABLE_ITEMS.keys():
+            return -1
+
+def _extract_price(item, amount) -> int:
+    """
+    Returns price for a given item in a given amount
+    
+    >>> _extract_price("A", 2)
+    100
+    """
+    if item not in AVAILABLE_ITEMS.keys():
+        return -1
+    
+    discounted_price = 0
+    for offer in ALL_OFFERS:
+        if item == offer.item:
+            if amount >= offer.amount:
+                # Divide to find out how many offers are possible
+                number_offers = floor(amount / offer.amount)
+                discounted_price += offer.new_price * number_offers
+                amount -= number_offers * offer.amount
+
+    return amount * AVAILABLE_ITEMS[item] + discounted_price
+    
+    
 
 if __name__ == "__main__":
     checkout("A B B A A A")
