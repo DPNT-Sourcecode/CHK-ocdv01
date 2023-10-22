765cdd10a104d568a29124e3235dbc35b0ce228c
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2023-10-22 09:54:10 +0100

    Sun Oct 22 09:54:10 WEST 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,13 @@
+AVAILABLE_ITEMS = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15
+}
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     raise NotImplementedError()
+
